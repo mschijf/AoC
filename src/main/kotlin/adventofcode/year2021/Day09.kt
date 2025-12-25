178bbbad88e10ae7415e6cc010fa446c8cbb629c
martinschijf <martinschijf@bol.com>
2025-12-25 11:21:17 +0100

    Adding 2021-08 (rewritten in Kotlin after originally started in Java)

---
+++ src/main/kotlin/adventofcode/year2021/Day09.kt
@@ -0,0 +1,50 @@
+package adventofcode.year2021
+
+import adventofcode.PuzzleSolverAbstract
+import tool.coordinate.twodimensional.Point
+
+fun main() {
+    Day09(test=true).showResult()
+}
+
+class Day09(test: Boolean) : PuzzleSolverAbstract(test, puzzleName="TBD", hasInputFile = true) {
+
+    private val caveMap = inputLines.asGrid().mapValues { (key, ch) -> ch.digitToInt() }
+    val minX = caveMap.keys.minOf { it.x }
+    val maxX = caveMap.keys.maxOf { it.x }
+    val minY = caveMap.keys.minOf { it.y }
+    val maxY = caveMap.keys.maxOf { it.y }
+
+    override fun resultPartOne(): Any {
+        return caveMap.keys.filter { it.isLowest() }.sumOf { lowPoint -> lowPoint.riskValue()}
+    }
+
+    override fun resultPartTwo(): Any {
+        return caveMap.keys.filter { it.isLowest() }
+            .map { it.basinSize() }
+            .sortedDescending()
+            .take (3)
+            .fold (1) { acc, basinSize -> acc * basinSize }
+    }
+
+    private fun Point.isLowest(): Boolean =
+        this.neighbors().filter { nb -> nb.inCave() }.all{ nb -> caveMap[nb]!! > caveMap[this]!!}
+
+    private fun Point.inCave(): Boolean =
+        this.x in minX..maxX && this.y in minY .. maxY
+
+    private fun Point.riskValue() =
+        caveMap[this]!! + 1
+
+    private fun Point.basinSize(basin: MutableSet<Point> = mutableSetOf()): Int {
+        if (this in basin)
+            return 0
+
+        basin += this
+        return 1 + this.neighbors()
+            .filter { nb -> nb.inCave() && caveMap[nb]!! < 9 }
+            .sumOf { nb -> nb.basinSize(basin) }
+    }
+}
+
+
